b8ddd7ea3b8523d9340cbe9612fcabba08c66383
Riccardo Andreazza <riccardo.andreazza90@gmail.com>
2023-11-27 17:59:54 +0100

    add: header

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -5,8 +5,26 @@ export default {
 </script>
 
 <template>
-<h2>Header</h2>
+<header>
+    <img src="../assets/img/Yugioh_anime_logo.webp" alt="">
+    <h1>Yu-Gi-Oh Api</h1>
+</header>
 </template>
 
 <style scoped lang="scss">
+header {
+    height: 100px;
+    display: flex;
+    align-items: center;
+    padding: .5rem 1.5rem;
+    gap: 1rem;
+    
+    h1 {
+        font-size: 4rem;
+    }
+
+    img {
+        max-height: 50%;
+    }
+}
 </style>
