8f6ca832a5c0edd7c07cbf781f7953b65c7c0ffe
Riccardo Andreazza <riccardo.andreazza90@gmail.com>
2023-11-27 18:11:08 +0100

    add: select section

--- src/components/AppSelect.vue
+++ src/components/AppSelect.vue
@@ -5,8 +5,24 @@ export default {
 </script>
 
 <template>
-    <h2>Select</h2>
+    <section>
+        <div class="container">
+            <select class="form-select fw-bolder" name="deck-list" id="decks">
+                <option value="alien">Alien</option>
+            </select>
+        </div>
+    </section>
 </template>
 
 <style scoped lang="scss">
+@use "../style/general.scss";
+@use "../style/partials/variables" as *;
+section {
+    padding: 1rem;
+    background-color: $bg-yellow;
+
+    select {
+        max-width: 200px;
+    }
+}
 </style>
