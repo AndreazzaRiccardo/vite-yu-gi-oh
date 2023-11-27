96311a26a00647a9b68fedc81556cb86e31e72e8
Riccardo Andreazza <riccardo.andreazza90@gmail.com>
2023-11-27 18:54:21 +0100

    add:card list section

---
+++ src/components/AppCard.vue
@@ -0,0 +1,32 @@
+<script>
+export default {
+    props: {
+        card: Object
+    }
+}
+</script>
+
+<template>
+    <div class="card">
+        <img :src="card.card_images[0].image_url" class="card-img-top" alt="...">
+        <div class="card-body">
+            <h5 class="card-text text-center">{{ card.name.toUpperCase() }}</h5>
+            <p class="text-center fw-bold">{{ card.archetype }}</p>
+        </div>
+    </div>
+</template>
+
+<style scoped lang="scss">
+@use "../style/general.scss";
+@use "../style/partials/variables" as *;
+.card {
+    .card-body {
+        background-color: $bg-yellow;
+        height: 120px; 
+
+        h5 {
+            color: white;
+        }
+    }
+}
+</style>
--- src/components/AppCardList.vue
+++ src/components/AppCardList.vue
@@ -1,12 +1,53 @@
 <script>
+import AppCard from './AppCard.vue';
+import { store } from '../store';
 export default {
-
+components: {
+    AppCard
+},
+data() {
+    return {
+        store,
+    }
+}
 }
 </script>
 
 <template>
-<h2>List</h2>
+<main>
+    <div class="container p-5">
+        <div class="total">Found {{ store.cardList.length }} cards</div>
+        <div class="row row-cols-5">
+           <div class="col g-4" v-for="card in store.cardList">
+            <AppCard :card="card"/>
+           </div>
+        </div>
+    </div>
+</main>
 </template>
 
 <style scoped lang="scss">
+@use "../style/general.scss";
+@use "../style/partials/variables" as *;
+
+main {
+    background-color: $bg-yellow;
+
+    .container {
+        background-color: white;
+
+        .total {
+            width: 100%;
+            height: 80px;
+            color: white;
+            background-color: rgb(44, 44, 44);
+            padding: 1rem;
+            display: flex;
+            align-items: center;
+            justify-content: flex-start;
+            font-weight: bolder;
+            font-size: 1.3rem;
+        }
+    }
+}
 </style>
